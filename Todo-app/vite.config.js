b95c5196e2748f52f8abad26d606dedb2fec2a43
Rahul Mandal <rahul@example.com>
2025-05-11 01:01:42 +0530

    Fix Vite base for GitHub Pages

--- Todo-app/vite.config.js
+++ Todo-app/vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/routine-todo/", // 👈 ye line zaroori hai
   plugins: [react()],
-base: "/routine-todo",
-
 })
+
